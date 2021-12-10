65f944f0dfd8130a7aeff26207810916c7c44855
kjw <jwkim@idino.co.kr>
2021-12-10 11:40:25 +0900

    12 -10

--- leetcode/21. Merge Two Sorted Lists.js
+++ leetcode/21. Merge Two Sorted Lists.js
@@ -1,15 +1,16 @@
-//--runtime error :P---
+//--runtime error :P--- #1
+/* 
 var mergeTwoLists = function (list1, list2) {
   var output = [];
   if (list1.length * list2.length == 0) {
-    console.log(output,'if');
+    console.log(list1.concat(list2), "if");
 
     return list1.concat(list2);
   } else {
     for (var i = 0; Math.max(list1.length, list2.length); i++) {
       if (list1[i] == undefined || list2[i] == undefined) {
         if (list1.length == list2.length) {
-          console.log(output,'elseif');
+          console.log(output, "elseif");
           return output;
         } else if (list1.length > list2.length) {
           return output.concat(list1.slice(i));
@@ -27,14 +28,34 @@ var mergeTwoLists = function (list1, list2) {
       }
     }
   }
-  console.log(output,'final');
+  console.log(output, "final");
   return output;
 };
+*/
 
-mergeTwoLists([1, 2, 4], [1, 3, 4]);
+//--runtime error :P--- #2
+/*
+var mergeTwoLists = function (list1, list2) {
+  var a = [];
+  for (var i = 0; i < list1.length; i++) {
+    if (list2[i] == undefined) {
+      break;
+    }
+    a.push([list1[i], list2[i]]);
+  }
+  if (list1.length > list2.length) {
+    a.push(list1.slice(i));
+  } else if (list1.length < list2.length) {
+    a.push(list2.slice(i));
+  }
+  console.log(a.map((x)=>x.sort((y,z)=>y-z)).flat())
+  return a.map((x) => x.sort((y, z) => y - z)).flat();
+};
 
-//Output: [1,1,2,3,4,4]
-mergeTwoLists([], []);
+mergeTwoLists([1, 2, 4], [1, 3, 4]); //Output: [1,1,2,3,4,4]
+mergeTwoLists([], []); // []
+mergeTwoLists([], [0]); //[0]
+*/
 
 /*
 
